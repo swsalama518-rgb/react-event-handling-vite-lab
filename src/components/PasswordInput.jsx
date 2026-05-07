a3d49c8d67d2726757461c783e09c94b5d806eb0
salma <swsalama518@gmail.com>
2026-05-07 01:38:26 -0400

    Passowrd app

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,15 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    const handleTyping = (event) => {
+        console.log("User is typing: ", event.target.value);
+    }
     return(
-        <></>
+        <input type="password" 
+        onChange={handleTyping} 
+        />
     )
+    
 }
 
 export default PasswordInput;
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,19 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    const handleMouseEnter = () => {
+        console.log("Mouse entered the button!");
+    };
+    const handleMouseLeave = () => {
+        console.log("Mouse left the button!");
+    };
     return(
-        <></>
+        <button 
+        onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave}
+        >
+            Submit Password
+        </button>
     )
 }
 
